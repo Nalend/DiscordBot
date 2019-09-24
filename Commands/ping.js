56797202f1e021c8c660fbbf8a5323846997b62f
Nalend <nalendrasatw@gmail.com>
2019-09-24 10:53:08 +0700

    Version 1.0.0

---
+++ Commands/ping.js
@@ -0,0 +1,11 @@
+const discord = require("discord.js");
+
+module.exports.run = async (bot, message, args)=> {
+
+    return message.channel.send("Manggil Terus kenapa Sayang !!! ");
+}
+
+module.exports.help ={
+    name: "ping"
+}
+
---
+++ bot.js
@@ -0,0 +1,43 @@
+const Discord = require("discord.js");
+const config = require("./config.json");
+const bot = new Discord.Client({disableEveryone: true});
+const fs = require("fs");
+bot.commands = new Discord.Collection();
+
+fs.readdir("./commands/",(err, files) => {
+    if(err) console.log(err);
+
+    let jsfile = files.filter(f => f.split(".").pop() === "js")
+    if(jsfile.length <= 0){
+        console.log("Gak ada Perintah !!!")
+        return;
+    }
+
+    jsfile.forEach((f, i) => {
+        let props = require(`./commands/${f}`)
+        console.log(`${f} loaded!`)
+        bot.commands.set(props.help.name, props);
+    })
+})
+
+
+bot.on("ready", async () => {
+    console.log(`${bot.user.username} is online!`)
+    bot.user.setActivity("Prefix is '?'!", {type: "WATCHING"});
+});
+//jika user dm chat di bot .. dia gak akan balas
+bot.on("message", async message => {
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+
+    let prefix = config.prefix;
+    let messageArray = message.content.split(" ")
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+
+    let commandfile = bot.commands.get(cmd.slice(prefix.length));
+    if(commandfile) commandfile.run(bot, message, args);
+})
+
+
+bot.login(config.token);
